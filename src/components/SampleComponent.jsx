b10bbe51b424c47b40d58b468cddd81b675f52bf
JD <j@mesjohnson.com>
2018-05-27 17:30:25 -0700

    Added Thunk & Hot Reloading, Updated package.json

--- src/components/SampleComponent.jsx
+++ src/components/SampleComponent.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 
 const SampleComponent = props => {
   return (
-    <div  style={{ textAlign: 'center' }} className="sample-component">
+    <div  style={{ textAlign: 'center', color: 'lightcoral' }} className="sample-component">
       <h1>ISSA Working!</h1>
     </div>
   );
